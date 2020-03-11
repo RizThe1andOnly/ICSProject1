7cafd920c400703a12baf1f8b00fbc1cc5c9a055
RizThe1andOnly <rizwanchowdhury111@gmail.com>
2020-03-11 00:30:14 -0400

    will continue working on it

--- Problem3v2.c
+++ Problem3v2.c
@@ -5,67 +5,100 @@
 
 //global constants to be used by functions
 const A = 2, B = 4, C = 6, D = 10;
+typedef struct tree_node
+{
+        unsigned num_children;
+        char name[10];
+        pid_t pid;
+        struct node* children;
+} node;
 
 int main(){
-    //create process A:
+
     pid_t pid_a, pid_b, pid_c, pid_waited_for;
-    int status;
+    int status, counter = 0;
 
-    pid_a = fork();
-    if(pid_a<0){//error
-        printf("Error in creating process A\n");
-    }
-    else if(pid_a == 0){//in process A:
-        printf("Process A with PID: %ld or %ld has been created\n",(long)getpid,(long)pid_a);
-        sleep(5);
-        //create process B and C
-        pid_b = fork();
-        if(pid_b != 0) pid_c = fork();
+    //create process tree structure:
+    //A
+    node *processA = (node*)malloc(sizeof(node));
+    processA->num_children = 2;
+    strcpy(processA->name,"Process A");
+    processA->children = NULL;
+    //B
+    node *processB = (node*)malloc(sizeof(node));
+    processB->num_children = 1;
+    strcpy(processB->name,"Process B");
+    processB->children = NULL;
+    //C
+    node *processC = (node*)malloc(sizeof(node));
+    processC->num_children = 0;
+    strcpy(processC->name,"Process C");
+    processC->children = NULL;
+    //D
+    node *processD = (node*)malloc(sizeof(node));
+    processD->num_children = 0;
+    strcpy(processD->name,"Process D");
+    processD->children = NULL;
+    //A-> B & C
+    node *bandc[2] = {processB,processC};
+    processA->children = bandc;
+    // B->D
+    processB -> children = processD;
 
-        //deal with different cases for Processes B and C
-        if((pid_b < 0) || (pid_c < 0)){ //either or both had error in creation
-            if(pid_b < 0 && pid_c < 0 ){ // both less than zero -> failed to create
-                printf("Processes B and C have failed to start\n");
-            }
-            else{
-                char processName = (pid_b < 0 ) ? "B":"C";
-                printf("Process %c has failed to start.\n");
-            }
-        }
-        else if(pid_b == 0){ //in process B
-            printf("Process B with PID: %ld has started.\n",(long)getpid());
-            sleep(5);
-            printf("Process B will now create Process D.\n");
-            pid_t pid_d = fork();
-            if(pid_d < 0 ){ //error in process creation
-                printf("Process D has failed to start. \n");
-            }
-            else if(pid_d == 0){ // in process D
-                printf("Process D with PID: %ld has started.\n",(long)getpid());
-                sleep(10);
-                exit(D);
-            }
-            // now in Process B, will do nothing now and exit the if statements to the wait statemetns below
-        }
-        else if(pid_c == 0){// in process C
-            printf("Process C with PID: %ld has strated.\n",(long)getpid());
-            sleep(5);
-            //process C will now exit out of these if statements and go to waiting block
-        }
-        // process A will now exit out of if statement block to go to waiting block
-    }
+    //traverse tree and create processes:
+    traverseTree(processA,&(processA->pid));
 
-    //waiting block
+    //wait for processes to complete:
     do
     {
         pid_waited_for = wait(&status);
-        explain_wait_status(pid_waited_for,&status);
+        explain_wait_status(pid_waited_for,status);
     } while ((!pid_waited_for));
     
-
 }
 
+/* function to traverse tree and create processes accordingly */
+void traverseTree(node* root, pid_t *pidLoc){
+    if(root == NULL) return; //base case
 
+    //create new process:
+    pid_t pid = fork();
+
+    //deal with different cases for possible cases for pid
+    if(pid<0){//error in creating process
+        printf("%s failed to start.\n",root->name);
+    }
+    else if(pid == 0){//in child process
+        //has more than one child
+        if(root->num_children > 1){
+            node** kids = root->children;
+            for(int i=0;i<(root->num_children);i++){
+                node *focusnode = *(kids+i);
+                traverseTree(focusnode,&(focusnode->pid));
+            }
+        }
+        else if((root->num_children)==1){
+            node* focusnode = root->children;
+            traverseTree(focusnode,&(focusnode->pid));
+        }
+    }
+    else{// in parent process
+        *(pidLoc) = pid;
+        return;
+    }
+
+    //wait for processes to complete:
+    int status,counter = 0;
+    pid_t pid_waited_for;
+    do
+    {
+        pid_waited_for = wait(&status);
+        explain_wait_status(pid_waited_for,status);
+        if(!pid_waited_for) counter++;
+    } while (counter<(root->num_children));
+
+    exit(8);
+}
 
 
 /* 
--- Problem3v3.c
+++ Problem3v3.c
@@ -12,9 +12,10 @@ const A = 2, B = 4, C = 6, D = 10;
 
 typedef struct tree_node
 {
+        struct node *children;
         unsigned num_children;
         char name[6];
-        struct node* children;
+        pid_t pid;
 } node;
 
 int main(){
@@ -25,7 +26,7 @@ int main(){
 
     //create nodes B and C
     //here i=0 will be to create B and i=1 will be to create C
-    node * bandc[2];
+    node  *bandc[2];
     char nameofnode[6] = "";
     for(int i=0;i<2;i++){
         int numofchildren = (i==0) ? 1:0;
@@ -35,12 +36,23 @@ int main(){
         else {
             strcpy(nameofnode,"nodeC");
         }
-        bandc[i] = &(node){numofchildren,nameofnode,NULL};
+        node *tempNode = (node*)malloc(sizeof(node));
+        tempNode->children = numofchildren;
+        strcpy(tempNode->name,nameofnode);
+        tempNode->children = NULL;
+        bandc[i] = tempNode;
     }
-    A->children = &bandc[0];
+    
+    A->children = bandc;
+    node** temp = A->children;
+    node* temp2 = *(temp+1);
 
-    for(int i=0;i<2;i++){
-        printf("%s\n",((node*)A->children+i)->name);
+    temp2 -> pid = fork();
+    if(temp2->pid == 0){
+        strcpy(temp2->name,"hugabuga");
     }
+    printf("%ld\n",(long)temp2->pid);
+    printf("%s\n",temp2->name);
+
     
 }
