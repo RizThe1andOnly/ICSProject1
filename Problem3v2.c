5944548a017b48897df75fdb50ffd5b96e9dadcc
RizThe1andOnly <rizwanchowdhury111@gmail.com>
2020-03-13 03:46:04 -0400

    finished version

--- Problem3v2.c
+++ Problem3v2.c
@@ -4,7 +4,15 @@
 #include <sys/wait.h>
 
 //global constants to be used by functions
-const A = 2, B = 4, C = 6, D = 10;
+const int A = 2;
+const int B = 4;
+const int C = 6;
+const int D = 10;
+const int WAIT_TIME = 20;
+
+//const pointer to file to put output in
+FILE *Q3OutputFile;
+
 typedef struct tree_node
 {
         unsigned num_children;
@@ -16,6 +24,10 @@ typedef struct tree_node
 char getProcessName(int exitcode);
 
 int main(){
+
+    //initialize File pointer
+    //Q3OutputFile = fopen("Q3OutputFile.txt","a"); //will use "a" to append to same file
+    Q3OutputFile = stderr; // outputs to standard output console
 
     pid_t pid_a, pid_b, pid_c, pid_waited_for;
     int status, counter = 0;
@@ -53,7 +65,8 @@ int main(){
     rootObject -> pid = getpid();
 
     //print root info:
-    printf("Main/Root process from which tree will start has PID: %ld\n",(long)(rootObject->pid));
+    fprintf(Q3OutputFile,"Main/Root process from which tree will start has PID: %ld\n",(long)(rootObject->pid));
+    fflush(Q3OutputFile);
 
     //traverse tree and create processes:
     traverseTree(processA,&(processA->pid),rootObject);
@@ -64,7 +77,9 @@ int main(){
         pid_waited_for = wait(&status);
         if(pid_waited_for != -1) explain_wait_status(pid_waited_for,status);
     } while (pid_waited_for != -1);
-    
+
+    //print tree with aux function:
+    print_tree(processA);
 }
 
 /* function to traverse tree and create processes accordingly */
@@ -98,13 +113,15 @@ void traverseTree(node* root, pid_t *pidLoc, node* parent){
 
     //deal with different cases for possible cases for pid
     if(pid<0){//error in creating process
-        printf("%s failed to start.\n",root->name);
+        fprintf(Q3OutputFile,"%s failed to start.\n",root->name);
+        fflush(Q3OutputFile);
     }
     else if(pid == 0){//in child process
 
         //print start of process
-        printf("%s with PID: %ld has started. From parent %s\n",(root->name),(long)getpid(),(parent->name));
-        sleep(10); //sleep to slow down process in order to better observe
+        fprintf(Q3OutputFile,"%s with PID: %ld has started. From parent %s\n",(root->name),(long)getpid(),(parent->name));
+        fflush(Q3OutputFile);
+        sleep(20); //sleep to slow down process in order to better observe
         //has more than one child
         if(root->num_children > 1){
             node** kids = root->children;
@@ -118,7 +135,7 @@ void traverseTree(node* root, pid_t *pidLoc, node* parent){
             traverseTree(focusnode,&(focusnode->pid),root);
         }
         else{//no children
-            sleep(1);
+            sleep(WAIT_TIME);
             exit(exitcode);
         }
     }
@@ -128,7 +145,8 @@ void traverseTree(node* root, pid_t *pidLoc, node* parent){
     }
 
     //wait for processes to complete:
-    printf("%s will wait for its children to terminate.\n",(root->name));
+    fprintf(Q3OutputFile,"%s will wait for its children to terminate.\n",(root->name));
+    fflush(Q3OutputFile);
     int status,counter = 0;
     pid_t pid_waited_for;
     do
@@ -148,27 +166,28 @@ void traverseTree(node* root, pid_t *pidLoc, node* parent){
 void explain_wait_status(pid_t pid, int status){
     if(WIFEXITED(status)){
         char processName = getProcessName(WEXITSTATUS(status));
-        fprintf(stderr,"Child Process %c with PID= %ld terminated normally, exit status = %d\n",
+        fprintf(Q3OutputFile,"Child Process %c with PID= %ld terminated normally, exit status = %d\n",
                 processName,(long)pid,WEXITSTATUS(status));
     }
     else if(WIFSIGNALED(status)){
-        fprintf(stderr,"Child with PID = %ld was terminated by signal, signal = %d\n",
+        fprintf(Q3OutputFile,"Child with PID = %ld was terminated by signal, signal = %d\n",
                 (long)pid,WTERMSIG(status));
     }
     else if(WIFSTOPPED(status)){
-        fprintf(stderr,"Child with PID = %ld has been stopped by a signal, signo = %d\n",
+        fprintf(Q3OutputFile,"Child with PID = %ld has been stopped by a signal, signo = %d\n",
                 (long)pid,WSTOPSIG(status));
     }
     else{
-        fprintf(stderr,"%s: Internal error: Unhandled case, PID = %ld, status = %d\n",
+        fprintf(Q3OutputFile,"%s: Internal error: Unhandled case, PID = %ld, status = %d\n",
                 __func__, (long)pid, status);
         exit(1);
     }
-    fflush(stderr);
+    fflush(Q3OutputFile);
 }
 
 /* function to obtain Process name from exit code */
 char getProcessName(int exitcode){
+    
     switch(exitcode){
         case 2:
             return 'A';
@@ -184,3 +203,35 @@ char getProcessName(int exitcode){
 
     return 'R';
 }
+
+
+/* Auxillary function provided by instructor to print tree. This will call
+   method below to print */
+void print_tree(node* root){
+    fprintf(Q3OutputFile,"\n\n");
+    __print_tree(root,1);
+    fflush(Q3OutputFile);
+}
+
+/* Aux function to print tree, will take level and node as input and print*/
+void __print_tree(node* root, int level){
+    for(int i=0;i<level;i++){
+        fprintf(Q3OutputFile,"\t");
+    }
+    fprintf(Q3OutputFile,"%s\n",root->name);
+    fflush(Q3OutputFile);
+
+    if((root->num_children) == 1){
+           __print_tree(root->children,level+1);
+           fflush(Q3OutputFile);
+       }
+    else{
+        node** kids = root->children;
+        for(int i=0;i<(root->num_children);i++){
+            node *focusnode = *(kids+i);
+            __print_tree(focusnode,level+1);
+            fflush(Q3OutputFile);
+        }
+    }
+}
+
